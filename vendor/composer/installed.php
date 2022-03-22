dfcd99affdfb029ad740571e3771048874de9e53
Julio Cesar JR <jcjunior92.jj@gmail.com>
2022-03-22 13:25:54 -0300

    Composer e Autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '34650a97b323806920d5378b6c2944188fa12079',
+        'name' => 'vendor/aas',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/aas' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '34650a97b323806920d5378b6c2944188fa12079',
+            'dev_requirement' => false,
+        ),
+    ),
+);
